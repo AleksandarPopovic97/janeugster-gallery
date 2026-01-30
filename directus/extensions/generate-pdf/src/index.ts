53344265708c07fb8162e1c5f0d83b07aec5d6ae
Aleksandar <admin@webueno.com>
2026-01-30 17:40:44 +0100

    Extensions added

---
+++ directus/extensions/generate-pdf/src/index.ts
@@ -0,0 +1,6 @@
+import { defineEndpoint } from '@directus/extensions-sdk';
+
+export default defineEndpoint((router) => {
+	router.get('/', (_req, res) => res.send('Hello, World test!'));
+});
+
---
+++ directus/extensions/print/src/module.vue
@@ -0,0 +1,21 @@
+<template>
+	<private-view title="My Custom Module">
+		<button @click="handlePrint">PrintSYNC</button>
+		<div ref="componentRef">
+			<h1>Hello, world SYNC!</h1>
+		</div>
+	</private-view>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useVueToPrint } from "vue-to-print";
+
+const componentRef = ref();
+const { handlePrint } = useVueToPrint({
+  content: componentRef,
+  documentTitle: "AwesomeFileName",
+});
+
+
+</script>
